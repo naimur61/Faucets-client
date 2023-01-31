8fd86304464abef0d0eb393f5daa87e8f2f634b7
Naimur <naimur.r61n@gmail.com>
2023-02-01 03:51:53 +0600

    recaptcha

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,24 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import React from 'react';
+import WarningIcon from '@mui/icons-material/Warning';
+import { Stack } from '@mui/system';
+import { useForm } from 'react-hook-form';
+import ReCAPTCHA from "react-google-recaptcha";
+import { useState } from 'react';
+
+
 
 const Home = () => {
+
+   const { register, handleSubmit, formState: { errors } } = useForm();
+   const onSubmit = data => console.log(data);
+   const [verified, setVerified] = useState(false);
+
+   const onChange = (value) => {
+      // console.log("Captcha value:", value);
+      setVerified(!verified)
+   }
+
    return (
       <Box sx={{
          background: '#EEF2FE',
@@ -36,8 +53,71 @@ const Home = () => {
 
             </Box>
 
+            {/* Form  */}
+            <Box sx={{
+               background: ' #fff',
+               border: ' 1px solid #eee',
+               padding: ' 25px 20px',
+            }}>
+
+               <Stack direction={{ xs: 'colum', md: 'row' }} spacing={2} alignItems='center' sx={{ background: '#eef2fe', padding: '10px' }}>
+                  <WarningIcon color='primary' />
+                  <Typography>Your wallet is connected to <strong>Ethereum Kovan</strong>, so you are requesting <strong>Ethereum Kovan</strong> Link/ETH.</Typography>
+               </Stack>
+
+               {/* Form Input  */}
+               <Box
+                  component="form" onSubmit={handleSubmit(onSubmit)} width={{ xs: '100%', md: '75%', lg: '50%' }}
+               >
+
+                  <Box marginY='10px'>
+                     <Typography component='label' fontSize='13px' fontWeight='600' color='primary' >Wallet Address</Typography>
+                     <TextField
+                        id="address"
+                        size="small"
+                        focused={false}
+                        fullWidth
+                        error={!!errors?.address}
+                        helperText={errors?.address?.message}
+                        placeholder='Your wallet address...'
+                        {...register("address", { required: "Please enter your wallet address." })}
+                     />
+                  </Box>
+
+                  <Typography component='label' htmlFor='request' fontSize='13px' fontWeight='600' color='primary' >Wallet Address</Typography>
+                  <Stack direction={{ xs: 'colum', md: 'row' }} justifyContent='space-between' spacing={3} marginBottom='10px'>
+                     <TextField
+                        id="request"
+                        size="small"
+                        focused={false}
+                        disabled
+                        fullWidth
+                        defaultValue='20 Test Link'
+                     />
+
+                     <TextField
+                        id="request"
+                        size="small"
+                        focused={false}
+                        disabled
+                        fullWidth
+                        defaultValue='0.5 ETH'
+                     />
+                  </Stack>
 
 
+                  <ReCAPTCHA
+                     sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+                     onChange={onChange}
+                  />
+
+
+                  <Button type="submit" variant="contained" sx={{ my: '20px' }} disabled={!verified}>Send Request</Button>
+               </Box>
+
+
+
+            </Box>
          </Box>
       </Box >
    );
